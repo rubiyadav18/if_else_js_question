5441373f72ebe783d2ba27cc28cab75994096d8b
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-11 20:45:04 +0530

    Add files via upload

---
+++ print_student_name.js
@@ -0,0 +1,31 @@
+let readlineSync=readlineSync.require("readline-Sync")
+var day=readlineSync.questionInt("enter a dayy--")
+
+var day = "TUeSday".toLowerCase();
+let readlineSync=require('readline-sync')
+var day=readlineSync.question("enter a day----")
+
+if (day == "monday") {
+  console.log("raani\nsonu\nrohit\nrubi\nMayuri\nGauri");
+}
+else if (day == "tuesday") {
+  console.log("Keemaya\nRoshni\nPooja\Priyanka\nnalini\nManisha");
+}
+else if (day == "wednesday") {
+  console.log("Shrusti\Karuna\Parveen\Sandhya\nLeena\nKarishma");
+}
+else if (day == "thursday") {
+  console.log("Nikita\nMona\nPriyanka\Tejashree\nLovely\nMehzebin");
+}
+else if (day == "friday") {
+  console.log("Pihu\Sejal\nChaya\Shrestha\nMonali\nAardhangya");
+}
+else if (day == "saturday") {
+  console.log("Gunjan\nShweta\nRani\nIsha\nKusu\Madhu\n");
+}
+else if (day == "sunday") {
+  console.log("Swati\nKhusboo\nidhi\Preeti\nDipti\nAnamika");
+}
+else {
+  console.log("Please enter valid week day")
+}
