099098d79eda9a70f06d3112f4c76e1c0c4305e3
Rubi Yadav <73836805+rubiyadav18@users.noreply.github.com>
2021-09-11 20:45:38 +0530

    Add files via upload

---
+++ ques6.js
@@ -0,0 +1,9 @@
+let readlineSync=require("readline-sync")
+var number=readlineSync.questionInt("enter a  number--")
+num=300-123;
+if (number==num){
+    console.log("it is equal",number)
+}
+else{
+    console.log("it is not equal")
+}
